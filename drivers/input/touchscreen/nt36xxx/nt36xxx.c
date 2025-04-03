a81f47ba307d44a1877730fe2062af56f098c42e
almostdead1 <ajoygandu78@gmail.com>
2025-04-03 18:54:37 +0530

    check again

--- drivers/input/touchscreen/nt36xxx/nt36xxx.c
+++ drivers/input/touchscreen/nt36xxx/nt36xxx.c
@@ -1592,6 +1592,7 @@ static int32_t nvt_ts_probe(struct i2c_client *client, const struct i2c_device_i
 		return -ENOMEM;
 	}
 
+    msleep(500);
     ts->gesture_enabled = 1;
 
 	ts->client = client;
