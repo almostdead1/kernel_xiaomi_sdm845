a8a1989de6de787e1274c038ac4a7d9744029a2d
almostdead1 <ajoygandu78@gmail.com>
2025-04-06 16:33:54 +0530

    needed by opsla

--- include/net/sock.h
+++ include/net/sock.h
@@ -343,6 +343,7 @@ struct sock {
 #define sk_incoming_cpu		__sk_common.skc_incoming_cpu
 #define sk_flags		__sk_common.skc_flags
 #define sk_rxhash		__sk_common.skc_rxhash
+#define op_sla_mark		__sk_common.skc_op_mark
 
 	socket_lock_t		sk_lock;
 	atomic_t		sk_drops;
