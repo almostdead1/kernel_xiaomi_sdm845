bb9b2a79b811556df207c19afaf38165e0362aca
almostdead1 <ajoygandu78@gmail.com>
2025-04-07 11:15:59 +0530

    okay!

--- include/trace/events/mmflags.h
+++ include/trace/events/mmflags.h
@@ -112,7 +112,7 @@ IF_HAVE_PG_MLOCK(PG_mlocked,		"mlocked"	)		\
 IF_HAVE_PG_UNCACHED(PG_uncached,	"uncached"	)		\
 IF_HAVE_PG_HWPOISON(PG_hwpoison,	"hwpoison"	)		\
 IF_HAVE_PG_IDLE(PG_young,		"young"		)		\
-IF_HAVE_PG_IDLE(PG_idle,		"idle"		)
+IF_HAVE_PG_IDLE(PG_idle,		"idle"		)		\
 /* CONFIG_SMART_BOOST add by bin.zhong@ASTI */	\
 IF_HAVE_PG_UIDRU(PG_uidlru,		"uidlru")
 
