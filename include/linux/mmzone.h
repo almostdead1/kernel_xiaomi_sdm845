d1b37651375e2281cfe321df9c0f8e464e179f80
almostdead1 <ajoygandu78@gmail.com>
2025-04-07 10:07:02 +0530

    needed for smartboost

--- include/linux/mmzone.h
+++ include/linux/mmzone.h
@@ -232,6 +232,16 @@ struct zone_reclaim_stat {
 	unsigned long		recent_scanned[2];
 };
 
+#ifdef CONFIG_SMART_BOOST
+struct uid_node {
+	struct uid_node __rcu *next;
+	uid_t uid;
+	unsigned int hot_count;
+	struct list_head  page_cache_list;
+	struct rcu_head rcu;
+};
+#endif
+
 struct lruvec {
 	struct list_head		lists[NR_LRU_LISTS];
 	struct zone_reclaim_stat	reclaim_stat;
@@ -240,6 +250,10 @@ struct lruvec {
 #ifdef CONFIG_MEMCG
 	struct pglist_data *pgdat;
 #endif
+#ifdef CONFIG_SMART_BOOST
+	struct uid_node **uid_hash;
+#endif
+
 };
 
 /* Mask used at gathering information at once (see memcontrol.c) */
