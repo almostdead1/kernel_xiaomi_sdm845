028a97659e0380e3c5b890149836bf18c4dfb43b
almostdead1 <ajoygandu78@gmail.com>
2025-04-24 22:43:56 +0530

    testing

--- include/linux/sched/mm.h
+++ include/linux/sched/mm.h
@@ -8,6 +8,12 @@
 #include <linux/mm_types.h>
 #include <linux/gfp.h>
 
+/* Grab a reference to a task's mm, if it is not already going away */
+extern struct mm_struct *get_task_mm(struct task_struct *task);
+
+
+/* mmput gets rid of the mappings and all user-space */
+extern void mmput(struct mm_struct *);
 
 
 #endif /* _LINUX_SCHED_MM_H */
--- include/linux/sched/task.h
+++ include/linux/sched/task.h
@@ -2,5 +2,15 @@
 #ifndef _LINUX_SCHED_TASK_H
 #define _LINUX_SCHED_TASK_H
 
+#define get_task_struct(tsk) do { atomic_inc(&(tsk)->usage); } while(0)
+
+
+extern void __put_task_struct(struct task_struct *t);
+
+static inline void put_task_struct(struct task_struct *t)
+{
+	if (atomic_dec_and_test(&t->usage))
+		__put_task_struct(t);
+}
 
 #endif /* _LINUX_SCHED_TASK_H */
