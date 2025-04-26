4df707791c08ad047bf350c62fe34a62806e9f16
almostdead1 <ajoygandu78@gmail.com>
2025-04-26 20:36:31 +0530

    testing

--- kernel/sched/sched.h
+++ kernel/sched/sched.h
@@ -1182,6 +1182,7 @@ DECLARE_PER_CPU(struct sched_domain *, sd_asym);
 DECLARE_PER_CPU(struct sched_domain *, sd_ea);
 DECLARE_PER_CPU(struct sched_domain *, sd_scs);
 DECLARE_PER_CPU(struct sched_domain *, sd_asym_cpucapacity);
+extern struct static_key_false sched_asym_cpucapacity;
 
 struct sched_group_capacity {
 	atomic_t ref;
